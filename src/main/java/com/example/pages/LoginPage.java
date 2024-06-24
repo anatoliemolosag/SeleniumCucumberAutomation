86a12a973327a26fe6131a113d4739530178f8f1
Anatolie Molosag <Anatolie@Roxanas-MacBook-Pro.local>
2024-06-24 13:42:37 -0500

    imports

--- src/main/java/com/example/pages/LoginPage.java
+++ src/main/java/com/example/pages/LoginPage.java
@@ -1,16 +1,8 @@
 package com.example.pages;
-
-import utils.BrowserUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 import java.util.List;
 
 public class LoginPage {
